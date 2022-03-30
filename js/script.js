8aa809df4d6a211c8aa43fef248d5da1c9cbd94c
Nicekode4 <45624383+Nicekode4@users.noreply.github.com>
2022-03-30 13:45:39 +0200

    ad

--- js/script.js
+++ js/script.js
@@ -1,8 +1,54 @@
-if (window.DeviceMotionEvent == undefined) {
-    //No accelerometer is present. Use buttons. 
-    alert("no accelerometer");
+/**
+ * @param callback function(error)
+ * @author YellowAfterlife
+ **/
+ function requestDeviceMotion(callback) {
+    if (window.DeviceMotionEvent == null) {
+        callback(new Error("DeviceMotion is not supported."));
+    } else if (DeviceMotionEvent.requestPermission) {
+        DeviceMotionEvent.requestPermission().then(function(state) {
+            if (state == "granted") {
+                callback(null);
+            } else callback(new Error("Permission denied by user"));
+        }, function(err) {
+            callback(err);
+        });
+    } else { // no need for permission
+        callback(null);
+    }
 }
-else {
-    alert("accelerometer found");
-    window.addEventListener("devicemotion", accelerometerUpdate, true);
+function firstClick() {
+    requestDeviceMotion(function(err) {
+        if (err == null) {
+            window.removeEventListener("click", firstClick);
+            window.removeEventListener("touchend", firstClick);
+            window.addEventListener("devicemotion", function(e) {
+                // access e.acceleration, etc.
+            });
+        } else {
+            // failed; a JS error object is stored in `err`
+        }
+    });
+}
+window.addEventListener("click", firstClick);
+window.addEventListener("touchend", firstClick);
+
+/**
+ * @param callback function(error)
+ * @author YellowAfterlife
+ **/
+ function requestDeviceOrientation(callback) {
+    if (window.DeviceOrientationEvent == null) {
+        callback(new Error("DeviceOrientation is not supported."));
+    } else if (DeviceOrientationEvent.requestPermission) {
+        DeviceOrientationEvent.requestPermission().then(function(state) {
+            if (state == "granted") {
+                callback(null);
+            } else callback(new Error("Permission denied by user"));
+        }, function(err) {
+            callback(err);
+        });
+    } else { // no need for permission
+        callback(null);
+    }
 }
